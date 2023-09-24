3cb40d6250ad7cedfaa07b1303979693b198a891
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-24 20:36:02 +0530

    memory game is now faster

--- memory-game/app.js
+++ memory-game/app.js
@@ -107,7 +107,7 @@ function flipCard() {
     this.setAttribute('src',cardArray[cardId].img)
 
     if(cardsChosen.length == 2){
-        setTimeout(checkMatch, 500)
+        setTimeout(checkMatch, 50)
     }
     
     
